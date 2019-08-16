cf4f5a2c8190f0e8a712851445319e4c4b47eb79
ArthurAbia <Arthurabia@gmail.com>
2019-08-16 15:56:22 +0100

    Redesigned, added interactivity, need functions now

--- calculator.js
+++ calculator.js
@@ -18,3 +18,36 @@ let operate = (operator, a, b) => {
       break;
   }
 }
+//Define all buttons and groups of buttons
+let buttons = document.getElementsByClassName('button');
+let operButtons = document.getElementsByClassName('oper');
+let resultDisplay = document.getElementsByClassName('result')[0];
+let inputDisplay = document.getElementsByClassName('input')[0];
+let clearButton = document.getElementsByClassName('clear')[0];
+let dotButton = document.getElementsByClassName('dot')[0];
+
+//Define the functions
+let toggleButtonAnimation = () => event.target.classList.toggle('clicked');
+let revertClass = () => event.target.classList.toggle('clicked');
+let displayNumber = () => {
+  if (inputDisplay.textContent.length < 15) {
+    inputDisplay.textContent += event.target.textContent;
+  }
+}
+let blockDecimals = () => {
+  if (inputDisplay.textContent.indexOf(".") > 0) {
+    event.target.removeEventListener("click", displayNumber);
+  }
+}
+
+Array.from(buttons).forEach( x => {
+  x.addEventListener("click", toggleButtonAnimation);
+  x.addEventListener("click", displayNumber);
+  x.addEventListener("transitionend", revertClass);
+})
+
+Array.from(operButtons).forEach( x => {
+  x.removeEventListener("click", displayNumber);
+})
+
+dotButton.addEventListener("click", blockDecimals);
